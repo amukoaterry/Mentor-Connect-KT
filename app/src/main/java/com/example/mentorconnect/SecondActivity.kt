094782b47f18552b6bff013d4532b942235d68f6
amukoaterry <amukoaterrymitchell@gmail>
2024-06-05 17:43:03 +0300

    Add Log In layout

--- app/src/main/java/com/example/mentorconnect/SecondActivity.kt
+++ app/src/main/java/com/example/mentorconnect/SecondActivity.kt
@@ -8,14 +8,13 @@ import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
-class MainActivity : AppCompatActivity() {
+class SecondActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        val connectRegister = findViewById<TextView>(R.id.textView2)
-            connectRegister.setOnClickListener{
-                val intent = Intent(this,SecondActivity::class.java)
-                startActivity(intent)
+        setContentView(R.layout.activity_second)
+        val connectRegister=findViewById<TextView>(R.id.textView4)
+        connectRegister.setOnClickListener {
+            finish()
         }
     }
 }
